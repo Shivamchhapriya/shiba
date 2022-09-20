bf0d935604f1c968d96c87d86d270ce746c4e7b2
Shivamchhapriya <88234621+Shivamchhapriya@users.noreply.github.com>
2022-09-20 16:03:33 +0530

    shiba

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+// import './App.css';
+// import Shiba_Ui from '../../../../../Downloads/images/shibaUI';
+import Shiba_Ui from './shibaUI';
+
+function App() {
+  return (
+   <>
+<Shiba_Ui/>
+   </>
+  );
+}
+
+export default App;
